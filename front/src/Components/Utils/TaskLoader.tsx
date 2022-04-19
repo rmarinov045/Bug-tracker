875fb7b83b43ca40e0a0cba755575ce6345f19fd
Radostin Marinov <r.marinov045@gmail.com>
2022-04-20 00:10:38 +0300

    Improving task loader xl screens placement

--- front/src/Components/Utils/TaskLoader.tsx
+++ front/src/Components/Utils/TaskLoader.tsx
@@ -3,7 +3,7 @@ import '../../App.css'
 
 function TaskLoader() {
     return (
-        <div className='w-full flex flex-col gap-10 items-center'>
+        <div className='w-full flex flex-col gap-10 items-center max-w-[60rem]'>
             <div className='rounded-xl p-2 bg-slate-300 w-2/3 h-24 mt-4'>
                 <div className='flex gap-2 h-full w-full p-2 animate-pulse'>
                     <div className='rounded-full w-14 h-14 bg-slate-400'></div>
